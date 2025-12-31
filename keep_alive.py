3369024f220b0453e1b156a29d6c0a21be0469ea
nwdcnnm-oss <nwdcnnm@gmail.com>
2025-12-31 11:08:28 +0700

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,24 +1,16 @@
 from flask import Flask
 from threading import Thread
-import logging
-
-# Vô hiệu hóa log của Flask để tránh làm rác màn hình Console
-log = logging.getLogger('werkzeug')
-log.setLevel(logging.ERROR)
 
 app = Flask('')
 
 @app.route('/')
 def home():
-    # Trang web tĩnh giúp Render nhận diện Bot vẫn đang hoạt động
-    return "Hệ thống Bot Buff Follow đang chạy 24/7!"
+    return "Bot status: Awake & Handling API Delays"
 
 def run():
-    # Cấu hình Host và Port tiêu chuẩn cho các dịch vụ Cloud
     app.run(host='0.0.0.0', port=8080)
 
 def keep_alive():
-    """Hàm khởi chạy luồng web song song với Bot"""
     t = Thread(target=run)
     t.daemon = True
     t.start()
