d56f863e2713e4f986299ea244679b8234461ec6
nwdcnnm-oss <nwdcnnm@gmail.com>
2025-12-31 17:31:30 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -19,7 +19,7 @@ WRONG_GROUP_MSG = (
     "👉 https://t.me/baohuydevs"
 )
 
-TIMEOUT = aiohttp.ClientTimeout(total=36)
+TIMEOUT = aiohttp.ClientTimeout(total=120)
 # =============================================
 
 # Hàm lấy ngày giờ Việt Nam hiện tại
